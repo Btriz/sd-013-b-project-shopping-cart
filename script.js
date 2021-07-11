4d7d58057ed95de7f7f232502e59e5fc5da23bcf
Btriz <00btrz@gmail.com>
2021-07-10 22:22:41 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -28,11 +28,11 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
+function cartItemClickListener() {
   // coloque seu código aqui
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
@@ -40,17 +40,51 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-/* const baseURL = 'https://api.mercadolibre.com/sites/MLB'
+async function getProducts() {
+  const query = 'search?q=computador';
+  const baseURL = `https://api.mercadolibre.com/sites/MLB/${query}`;
 
-async function getProducts () {
-  const response = await fetch(`${baseURL}/search?q=computador`);
-  const product = await response.json();
+  const response = await fetch(baseURL);
+  const products = await response.json();
+  return products.results;
 }
- */
+
+function addItemsToScreen(products) {
+  products.forEach((product) => {
+    const item = createProductItemElement(product);
+    const items = document.querySelector('.items');
+    items.appendChild(item);
+  });
+}
+
+// ----REQUISITO 2----
+function fetchProduct(itemId) {
+  return fetch(`https://api.mercadolibre.com/items/${itemId}`)
+    .then((response) => response.json())
+    .then((obj) => createCartItemElement(obj));
+}
+
+function addToCart(event) {
+  const item = event.target.parentNode;
+  const itemId = getSkuFromProductItem(item);
+
+  fetchProduct(itemId)
+  .then((obj) => {
+    const cart = document.querySelector('.cart__items');
+    cart.appendChild(obj);
+  });
+}
+
+function addToCartListener() {
+const buttons = document.querySelectorAll('.item__add');
+
+buttons.forEach((button) => {
+button.addEventListener('click', addToCart);
+});
+}
+
 window.onload = async () => {
-  /* const product = await getProducts();
-  product.results.forEach((product) => {
-    const element = createProductItemElement(product);
-    console.log(element);
-  }) */
+  const products = await getProducts();
+  addItemsToScreen(products);
+  await addToCartListener();
 };
